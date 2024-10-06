204cc7fedf547754779bd5c04db4c954ea67bd65
Sergio Barreras <sergiobg.isc@gmail.com>
2024-10-06 14:16:17 -0700

    feat: utilizar paquete en otros proyectos

--- src/app/modules/admin/layouts/admin-layout/admin-layout.component.ts
+++ src/app/modules/admin/layouts/admin-layout/admin-layout.component.ts
@@ -1,17 +1,20 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { SideMenuComponent } from '../../../shared/components/side-menu/side-menu.component';
+import { SbgSideMenuComponent, TitleColor } from 'sbg-side-menu';
+// import { SideMenuComponent } from '../../../shared/components/side-menu/side-menu.component';
 
 @Component({
   selector: 'app-admin-layout',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, SideMenuComponent],
+  imports: [CommonModule, RouterOutlet, SbgSideMenuComponent],
   templateUrl: './admin-layout.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export default class AdminLayoutComponent {
   isAuthenticated = signal(false);
+
+  TitleColor = TitleColor;
 
   onLogin() {
     this.isAuthenticated.set(true);
